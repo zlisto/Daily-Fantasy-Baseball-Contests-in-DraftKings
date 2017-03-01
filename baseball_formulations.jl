bd405288364f637ed0e8b9bcc5d526dc72de9c45
zlisto <zlisto@mit.edu>
2017-02-28 19:49:10 -0500

    Add files via upload

---
+++ baseball_formulations.jl
@@ -0,0 +1,140 @@
+#This code implements the formulations for  baseball lineups
+
+# To install DataFrames, simply run Pkg.add("DataFrames")
+using DataFrames
+
+# To install MathProgBase, simply run Pkg.add("MathProgBase")
+using MathProgBase
+# Once again, to install run Pkg.add("JuMP")
+using JuMP
+
+
+#=
+GLPK is an open-source solver.  For those that want to build
+very sophisticated models, they can buy Gurobi. To install GLPKMathProgInterface, simply run
+Pkg.add("GLPKMathProgInterface")
+=#
+using GLPKMathProgInterface
+
+#uncomment this line only if you installed Gurobi, which costs money :(), but is super fast :)
+#using Gurobi  
+
+
+
+
+#####################################################################################################################
+#####################################################################################################################
+# This is a function that creates one lineup using the  Stacking Type 3 formulation
+# No pitcher opposite batter
+# Batters with consecutive batting order
+#only keep 4th order and earlier batters, cuz they make more points
+function baseball_formulation(players, old_lineups, num_overlap,stack_size, P,B1,B2,B3,C,SS,OF, players_teams, players_opp, players_games,players_stacks)
+   
+	#number of players playing today
+    num_players = size(players)[1]
+    
+    #number of games today
+    games = unique(players[:Game])
+    num_games = size(games)[1]
+    
+    #number of teams playing today
+    teams = unique(players[:Team])
+    num_teams = size(teams)[1]
+
+    #number of stacks per team (this is 9)
+    num_stacks = 9;  
+    
+    #################################################################################################
+    #define the model formulation which will be solved using GLPK
+    m = Model(solver=GLPKSolverMIP())
+
+    #uncomment this line only if you are using Gurobi, which costs money :(), but is super fast :)
+    m = Model(solver=GurobiSolver(OutputFlag=0))
+
+
+    # Variable for players in lineup.
+    @defVar(m, players_lineup[i=1:num_players], Bin)
+    
+    #OBJECTIVE
+    @setObjective(m, Max, sum{players[i,:Proj_FP]*players_lineup[i], i=1:num_players})
+
+
+    #NUMBER OF PLAYERS: 10 players constraint
+    @addConstraint(m, sum{players_lineup[i], i=1:num_players} == 10)
+    
+    #SALARY: Financial Constraint - salary less than $50,000
+    @addConstraint(m, sum{players[i,:Salary]*players_lineup[i], i=1:num_players}<= 50000)
+
+    #POSITION
+    #  2 P constraint
+    @addConstraint(m, sum{P[i]*players_lineup[i], i=1:num_players}==2)
+    # one B1 constraint
+    @addConstraint(m, sum{B1[i]*players_lineup[i], i=1:num_players}==1)
+    # one B2 constraint
+    @addConstraint(m, sum{B2[i]*players_lineup[i], i=1:num_players}==1)
+    # one B3 constraint
+    @addConstraint(m, sum{B3[i]*players_lineup[i], i=1:num_players}==1)
+    # one C constraint
+    @addConstraint(m, sum{C[i]*players_lineup[i], i=1:num_players}==1)
+    # one SS constraint
+    @addConstraint(m, sum{SS[i]*players_lineup[i], i=1:num_players}==1)
+    # 3 OF constraint
+    @addConstraint(m, sum{OF[i]*players_lineup[i], i=1:num_players}==3)
+  
+   
+  
+    #GAMES: at least 2 different games for the 10 players constraints
+    #variable for each game used in lineup, equals 1 if game used
+    @defVar(m, used_game[i=1:num_games], Bin)
+    #constraint satisfied by each game
+    @addConstraint(m, constr[i=1:num_games], used_game[i] <= sum{players_games[t, i]*players_lineup[t], t=1:num_players})
+    #constraint makes sure at least 2 used_game variables equal 1
+    @addConstraint(m, sum{used_game[i], i=1:num_games} >= 2)
+
+
+    #at most 5 hitters from one team constraint
+    @addConstraint(m, constr[i=1:num_teams], sum{players_teams[t, i]*(1-P[t])*players_lineup[t], t=1:num_players}<=5)
+    
+
+    #OVERLAP Constraint
+    @addConstraint(m, constr[i=1:size(old_lineups)[2]], sum{old_lineups[j,i]*players_lineup[j], j=1:num_players} <= num_overlap)
+
+ 
+    #NO PITCHER VS BATTER no pitcher vs batter constraint
+    @addConstraint(m, hitter_pitcher[g=1:num_teams], 
+                   8*sum{P[k]*players_lineup[k]*players_teams[k,g],k=1:num_players} + 
+                    sum{(1-P[k])*players_lineup[k]*players_opp[k,g], k=1:num_players}<=8)
+
+
+    #STACK: at least stack_size hitters from at least 1 team, consecutive hitting order
+ 	#define a variable for each stack on each team.  This variable =1 if the stack on the team is used
+    @defVar(m, used_stack_batters[i=1:num_teams,j=1:num_stacks], Bin)
+    #constraint for each stack, used or not
+    @addConstraint(m, constraint_stack[i=1:num_teams,j=1:num_stacks], stack_size*used_stack_batters[i,j] <= 
+                   sum{players_teams[t, i]*players_stacks[t, j]*(1-P[t])*players_lineup[t], t=1:num_players})  
+    #make sure at least one stack is used
+    @addConstraint(m, sum{used_stack_batters[i,j], i=1:num_teams,j=1:num_stacks} >= 1)  
+	
+	
+
+
+	########################################################################################################
+    # Solve the integer programming problem  
+    tic()
+    status = solve(m);
+   
+
+    # Puts the output of one lineup into a format that will be used later
+    if status==:Optimal
+        players_lineup_copy = Array(Int64, 0)
+        for i=1:num_players
+            if getvalue(players_lineup[i]) >= 0.9 && getvalue(players_lineup[i]) <= 1.1
+                players_lineup_copy = vcat(players_lineup_copy, fill(1,1))
+            else
+                players_lineup_copy = vcat(players_lineup_copy, fill(0,1))
+            end
+        end
+
+        return(players_lineup_copy)
+    end
+end
---
+++ data_cleaning.jl
@@ -0,0 +1,529 @@
+#this file has a bunch of data cleaning functions
+using DataFrames
+
+
+
+################################################################################################################
+function clean_order(Order)  #cleans up the batting order so that it is a number
+    Order_clean =[];
+    for order in Order
+        if ~(typeof(order)==DataArrays.NAtype)
+            if isa(parse(order), Number) 
+                order_clean = parse(order); 
+            else
+                order_clean = 0;
+            end
+        else
+            order_clean=0;
+        end
+        Order_clean = [Order_clean;order_clean];
+    end
+    return Order_clean;
+end
+
+################################################################################################################
+# read_player_data loads information from hitters and pitchesr files into a single table
+function read_player_data(path_hitters,path_pitchers)
+    pitchers = readtable(path_pitchers);
+    hitters = readtable(path_hitters);
+
+
+    function clean_str(str)
+        if str[1]=='@'
+            return str[2:end];
+        else
+            return  str;
+        end
+    end
+
+    Team = [pitchers[:Team]; hitters[:Team]];
+    Opp=[pitchers[:Opp]; hitters[:Opp]];
+    Game = [];
+    for i = 1:size(Team)[1]
+        t1 = clean_str(Team[i]);
+        t2 = clean_str( Opp[i]);
+        t = sort([t1,t2]);
+        Game = [Game;string(t[1]," ",t[2])];
+    end
+
+    function clean_num(x)
+        if isnan(x)
+            return 0;
+        else
+            return  x;
+        end
+    end
+
+
+    p=map(clean_num, [pitchers[:Proj_FP]; hitters[:Proj_FP]]);
+    proj_val=map(clean_num, [pitchers[:Proj_Val]; hitters[:Proj_Val]]);
+    a=map(clean_num,[pitchers[:Actual_FP]; hitters[:Actual_FP]]);
+    players = DataFrame(Player_Name = [pitchers[:Player_Name]; hitters[:Player_Name]],
+                        Team = [pitchers[:Team]; hitters[:Team]],
+                        Opp=[pitchers[:Opp] ;hitters[:Opp]],
+                        Game = Game,
+                        Pos=[pitchers[:Pos] ;hitters[:Pos]],
+                        Salary=[pitchers[:Salary] ;hitters[:Salary]],
+                        Proj_FP=p,
+                        Proj_Val = proj_val,
+                        Actual_FP=a,
+                        Batting_Order_Confirmed_ = [round(Int,zeros(size(pitchers)[1])); clean_order(hitters[:Batting_Order_Confirmed_])]
+                        );
+
+    return players;
+end
+
+###################################################################################################
+# Output CSV file to upload to DraftKings
+function createOutputcsvFromTracer(tracer, players, P, C, oneB, twoB, threeB,SS,OF,path_to_output)
+    # should be in format P P C 1B 2B 3B SS OF OF OF
+    num_lineups = size(tracer)[2]
+    num_players =size(tracer)[1]
+    P_index = 1
+    C_index = 3
+    oneB_index = 4
+    twoB_index = 5
+    threeB_index = 6
+    SS_index = 7
+    OF_index = 8
+    GrowingLineupMatrix = "P,P,C,1B,2B,3B,SS,OF,OF,OF\n"
+    for j = 1:num_lineups
+        
+        lineup = ["" "" "" "" "" "" "" "" "" ""]
+        for i =1:num_players
+            if tracer[i,j] == 1
+                #println("\tlineup ", j," player ", i, ":", string(players[i,:Player_Name])," ",string(players[i,:Pos]))
+            
+                if P[i]==1  
+                    # if player i's position is P
+                    if lineup[P_index]==""
+                        lineup[P_index] = string(players[i,:Player_Name])
+                    elseif lineup[P_index+1] ==""
+                        lineup[P_index+1] = string(players[i,:Player_Name])
+                    else
+                        println(string("error in lineup",j,"player",i))
+                    end
+
+                elseif C[i]==1
+                    if lineup[C_index]==""
+                        lineup[C_index] = string(players[i,:Player_Name])
+                    else
+                        println(string("error in lineup",j,"player",i))
+                    end
+                elseif oneB[i]==1
+                    if lineup[oneB_index]==""
+                        lineup[oneB_index] = string(players[i,:Player_Name])
+                    else
+                        println(string("error in lineup",j,"player",i))
+                    end
+                elseif twoB[i]==1
+                    
+                    if lineup[twoB_index]==""
+                        lineup[twoB_index] = string(players[i,:Player_Name])
+                    else
+                        println(string("error in lineup",j,"player",i))
+                    end
+                elseif threeB[i]==1
+                    
+                    if lineup[threeB_index]==""
+                        lineup[threeB_index] = string(players[i,:Player_Name])
+                    else
+                        println(string("error in lineup",j,"player",i))
+                    end
+                elseif SS[i]==1
+                    
+                    if lineup[SS_index]==""
+                        lineup[SS_index] = string(players[i,:Player_Name])
+                    else
+                        println(string("error in lineup",j,"player",i))
+                    end
+                else
+                    if OF[i] != 1
+                        println(string("error in lineup",j,"player",i))
+                    end
+
+                    if lineup[OF_index]==""
+                        lineup[OF_index] = string(players[i,:Player_Name])
+                    elseif lineup[OF_index+1] ==""
+                        lineup[OF_index+1] = string(players[i,:Player_Name])
+                    elseif lineup[OF_index+2] ==""
+                        lineup[OF_index+2] = string(players[i,:Player_Name])
+                    else
+                        println(string("error in lineup",j,"player",i))
+                    end
+
+                end
+                
+            end
+        end
+       
+        
+        LineupOK = true
+        lineupRow =""
+        for name in lineup
+            lineupRow = string(lineupRow, name, ",")
+        end
+        
+        lineupRow = chop(lineupRow)  #remove extra comma
+        lineupRow = string(lineupRow,"\n")
+        GrowingLineupMatrix = string(GrowingLineupMatrix,lineupRow) 
+    end
+    
+
+    outfile = open(path_to_output, "w")
+    write(outfile, GrowingLineupMatrix)
+    close(outfile)
+    
+
+
+end
+
+###########################################################################################
+#####################################################################################################################
+#####################################################################################################################
+#this function will create the lineups and save them to a file
+function create_lineups(num_lineups, num_overlap, stack_size,formulation, path_pitchers,path_hitters,  path_to_output)
+    #=
+    num_lineups is an integer that is the number of lineups
+    num_overlap is an integer that gives the overlap between each lineup
+    stack_size = number of players in the stack
+    path_pitchers,path_hitters is a string that gives the path to the hitters and pitchers projections file
+    formulation is the type of formulation you would like to use (for instance one_lineup_Type_1, one_lineup_Type_2, etc.)
+    path_to_output is a string where the final csv file with your lineups will be
+    =#
+    
+    println("loading projection data")
+    players =read_player_data(path_hitters,path_pitchers);
+
+    
+    # Number of players
+    num_players = size(players)[1]
+    println(num_players," players playing tonight")
+
+
+    # Create team indicators from the information in the players file
+    teams = unique(players[:Team])
+    num_teams = size(teams)[1]
+    println(num_teams," teams playing tonight")
+
+    # Create team indicators from the information in the players file
+    games = unique(players[:Game])
+    num_games = size(games)[1]
+    println(num_games," games playing tonight")
+
+
+    # arrays that store the information on which players are which position
+    P = Array(Int64, 0);
+    B1 = Array(Int64, 0);
+    B2 =Array(Int64, 0);
+    B3 =Array(Int64, 0);
+    C =Array(Int64, 0);
+    SS=Array(Int64, 0);
+    OF=Array(Int64, 0);
+    RP=Array(Int64, 0);
+
+
+    #=
+    Process the position information in the skaters file to populate the position and order
+    #arrays  with the corresponding correct information
+    =#
+    for i =1:num_players
+        pos = players[i,:Pos];
+        #print(players[i,:Player_Name]," ",pos,"\n") 
+        if contains(pos,"SP") 
+            P=vcat(P,fill(1,1))
+            B1=vcat(B1,fill(0,1))
+            B2=vcat(B2,fill(0,1))
+            B3=vcat(B3,fill(0,1))
+            C=vcat(C,fill(0,1))
+            SS=vcat(SS,fill(0,1))
+            OF=vcat(OF,fill(0,1))
+            RP=vcat(RP,fill(0,1))
+
+
+        elseif contains(pos,"RP") 
+            P=vcat(P,fill(0,1))
+            RP=vcat(RP,fill(1,1))
+            B1=vcat(B1,fill(0,1))
+            B2=vcat(B2,fill(0,1))
+            B3=vcat(B3,fill(0,1))
+            C=vcat(C,fill(0,1))
+            SS=vcat(SS,fill(0,1))
+            OF=vcat(OF,fill(0,1))
+            
+        elseif contains(pos,"1B") 
+            P=vcat(P,fill(0,1))
+            B1=vcat(B1,fill(1,1))
+            B2=vcat(B2,fill(0,1))
+            B3=vcat(B3,fill(0,1))
+            C=vcat(C,fill(0,1))
+            SS=vcat(SS,fill(0,1))
+            OF=vcat(OF,fill(0,1))
+            RP=vcat(RP,fill(0,1))
+
+        elseif contains(pos,"2B") 
+            P=vcat(P,fill(0,1))
+            B1=vcat(B1,fill(0,1))
+            B2=vcat(B2,fill(1,1))
+            B3=vcat(B3,fill(0,1))
+            C=vcat(C,fill(0,1))
+            SS=vcat(SS,fill(0,1))
+            OF=vcat(OF,fill(0,1))
+            RP=vcat(RP,fill(0,1))
+
+        elseif contains(pos,"3B") 
+            P=vcat(P,fill(0,1))
+            B1=vcat(B1,fill(0,1))
+            B2=vcat(B2,fill(0,1))
+            B3=vcat(B3,fill(1,1))
+            C=vcat(C,fill(0,1))
+            SS=vcat(SS,fill(0,1))
+            OF=vcat(OF,fill(0,1))
+            RP=vcat(RP,fill(0,1))
+
+        elseif contains(pos,"C") 
+            P=vcat(P,fill(0,1))
+            B1=vcat(B1,fill(0,1))
+            B2=vcat(B2,fill(0,1))
+            B3=vcat(B3,fill(0,1))
+            C=vcat(C,fill(1,1))
+            SS=vcat(SS,fill(0,1))
+            OF=vcat(OF,fill(0,1))
+            RP=vcat(RP,fill(0,1))
+
+        elseif contains(pos,"SS") 
+            P=vcat(P,fill(0,1))
+            B1=vcat(B1,fill(0,1))
+            B2=vcat(B2,fill(0,1))
+            B3=vcat(B3,fill(0,1))
+            C=vcat(C,fill(0,1))
+            SS=vcat(SS,fill(1,1))
+            OF=vcat(OF,fill(0,1))
+            RP=vcat(RP,fill(0,1))
+
+        elseif contains(pos,"OF") 
+            P=vcat(P,fill(0,1))
+            B1=vcat(B1,fill(0,1))
+            B2=vcat(B2,fill(0,1))
+            B3=vcat(B3,fill(0,1))
+            C=vcat(C,fill(0,1))
+            SS=vcat(SS,fill(0,1))
+            OF=vcat(OF,fill(1,1))
+            RP=vcat(RP,fill(0,1))
+
+        else
+            println("\t",players[i,:Player_Name]," has no position\n")
+
+        end
+    end
+
+
+    #GAMES:   players_games stores information on which game each player is on
+    player_info = zeros(Int, num_games)
+
+    # Populate player_info with the corresponding information
+    for j=1:num_games
+        if players[1, :Game] == games[j]
+            player_info[j] =1
+        end
+    end
+    players_games = player_info'
+
+    for i=2:num_players
+        player_info = zeros(Int, num_games)
+        for j=1:num_games
+            if players[i, :Game] == games[j]
+                player_info[j] =1
+            end
+        end
+        players_games = vcat(players_games, player_info')
+    end
+
+    #TEAMS:   players_teams stores information on which team each player is on
+    player_info = zeros(Int, num_teams)
+
+    # Populate player_info with the corresponding information
+    for j=1:size(teams)[1]
+        if players[1, :Team] == teams[j]
+            player_info[j] =1
+        end
+    end
+    players_teams = player_info'
+
+    for i=2:num_players
+        player_info = zeros(Int, num_teams)
+        for j=1:size(teams)[1]
+            if players[i, :Team] == teams[j]
+                player_info[j] =1
+            end
+        end
+        players_teams = vcat(players_teams, player_info')
+    end
+
+    #OPPONENT TEAM players_opp stores information on which team each player is opposing
+    player_info = zeros(Int, num_teams)
+
+    # Populate player_info with the corresponding information
+    for j=1:num_teams
+        if players[1, :Opp]=='@'
+            opp = players[1, :Opp][2:end]
+        else
+            opp = players[1, :Opp]
+        end
+        
+        if opp == teams[j]
+            player_info[j] =1
+        end
+    end
+    players_opp = player_info'
+
+    for i=2:num_players
+        player_info = zeros(Int, num_teams)
+        for j=1:size(teams)[1]
+            if players[i, :Opp]=='@'
+                opp = players[i, :Opp][2:end]
+            else
+                opp = players[i, :Opp]
+            end
+            if opp == teams[j]
+                player_info[j] =1
+            end
+        end
+        players_opp = vcat(players_opp, player_info')
+    end
+
+    #STACK players_stacks stores information on which team each player is on
+    num_stacks = 9;  #number of stacks
+    
+    #make matrix for 1st stacking (stacking_order[1])
+    player_info = zeros(Int,num_stacks);
+
+    # Populate player_info with the corresponding information, start with the first player to initiate the array
+    for j=1:num_stacks
+        if players[1,:Batting_Order_Confirmed_] == j
+            stack_ind = circshift(collect(1:9),stack_order-j)[1:stack_order[1]];  #index of the stacks this batting order belongs to.  
+            #For ex., batting order 1, stacking order 3, will belong to (8,9,1),(9,1,2) and (1,2,3)
+            for k in stack_ind
+                player_info[k]=1;
+            end
+        end
+    end
+    players_stacks = player_info';
+
+    #now update the stack matrix of players 2 to num_players
+    for i=2:num_players
+        player_info = zeros(Int,num_stacks);
+        for j=1:num_stacks
+            if players[i,:Batting_Order_Confirmed_] == j
+                stack_ind = circshift(collect(1:9),num_stacks-j)[1:num_stacks];  #index of the stacks this batting order belongs to.  
+                #For ex., batting order 1, stacking order 3, will belong to (8,9,1),(9,1,2) and (1,2,3)
+                for k in stack_ind
+                    player_info[k]=1;
+                end
+            end
+        end
+        players_stacks = vcat(players_stacks, player_info');
+    end
+
+    ###########################################################################
+    #my formulation is:  formulation(players, old_lineups, num_overlap, stack_size,P,B1,B2,B3,C,SS,OF, players_teams, players_opp, players_games,players_stacks)
+
+    # Lineups using formulation as the stacking type
+    println("Calculating lineup 1 of ", num_lineups)
+           #baseball_formulation(players, old_lineups, num_overlap, P,B1,B2,B3,C,SS,OF, players_teams, players_opp, players_games,players_stacks, stack_size)
+    old_lineups =  hcat(zeros(Int, num_players), zeros(Int, num_players))
+    the_lineup  = formulation(players, old_lineups, num_overlap,stack_size, 
+                              P,B1,B2,B3,C,SS,OF, players_teams, players_opp, players_games,players_stacks)
+
+    println("Calculating lineup 2 of ", num_lineups)
+    old_lineups =hcat(the_lineup, zeros(Int, num_players))
+    the_lineup2 = formulation(players, old_lineups, num_overlap,stack_size, 
+                              P,B1,B2,B3,C,SS,OF, players_teams, players_opp, players_games,players_stacks)
+
+    old_lineups = hcat(the_lineup, the_lineup2)
+    for i=1:(num_lineups-2)
+        println("Calculating lineup ", i+2, " of ", num_lineups)
+        try
+            thelineup = formulation(players, old_lineups, num_overlap,stack_size, 
+                              P,B1,B2,B3,C,SS,OF, players_teams, players_opp, players_games,players_stacks)
+            old_lineups = hcat(old_lineups,thelineup)
+        catch
+            print("some optimization error")
+            break
+        end
+    end
+    
+    createOutputcsvFromTracer(old_lineups, players, P, C, B1, B2, B3,SS,OF,path_to_output)
+
+end
+###########################################################################################
+#save file with lineups and projected points
+function lineup_points_proj(path_lineups,path_hitters,path_pitchers,path_output)
+    players = read_player_data(path_hitters,path_pitchers)
+    lineups = readtable(path_lineups)
+    num_lineups = size(lineups)[1]
+    num_spots = size(lineups)[2]
+    num_players = size(players)[1]
+    GrowingLineupMatrix = "P,P,C,1B,2B,3B,SS,OF,OF,OF,Proj_pts\n"
+    for i = 1:num_lineups
+        lineup_pts = 0
+        for j = 1:num_spots
+            name = lineups[i,j]
+            for k = 1:num_players
+                if string(players[k,1])==string(name)
+                    lineup_pts  = lineup_pts+players[i,:Proj_FP]
+                end
+            end
+            GrowingLineupMatrix = string(GrowingLineupMatrix,name,",")
+        end
+        GrowingLineupMatrix = string(GrowingLineupMatrix, string(lineup_pts), "\n")
+    end
+    outfile = open(path_output, "w")
+    write(outfile, GrowingLineupMatrix)
+    close(outfile)
+
+end
+
+###########################################################################################
+#save file with lineups and projected and actual points
+function lineup_points_actual(path_lineups,path_hitters,path_pitchers,path_output)
+    players = read_player_data(path_hitters,path_pitchers)
+    lineups = readtable(path_lineups)
+    num_lineups = size(lineups)[1]
+    num_spots = size(lineups)[2]
+    num_players = size(players)[1]
+    GrowingLineupMatrix = "P,P,C,1B,2B,3B,SS,OF,OF,OF,Proj_pts,Actual_pts\n"
+    for i = 1:num_lineups
+        lineup_pts_proj = 0
+        lineup_pts_actual=0
+        for j = 1:num_spots
+            name = lineups[i,j]
+            for k = 1:num_players
+                if string(players[k,1])==string(name)
+                    lineup_pts_proj  = lineup_pts_proj + players[i,:Proj_FP]
+                    lineup_pts_actual  = lineup_pts_actual + players[i,:Actual_FP]
+
+                end
+            end
+            GrowingLineupMatrix = string(GrowingLineupMatrix,name,",")
+        end
+        GrowingLineupMatrix = string(GrowingLineupMatrix, string(lineup_pts_proj),",",string(lineup_pts_actual), "\n")
+    end
+    outfile = open(path_output, "w")
+    write(outfile, GrowingLineupMatrix)
+    close(outfile)
+
+end
+
+###########################################################################################
+#folder = "2016-08-12\\"  #folder where the player projections are
+#path_pitchers = string(folder,"dailyfantasynerd_pitchers.csv");
+#path_hitters = string(folder,"dailyfantasynerd_hitters.csv");
+#path_lineups = string(folder,"baseball_baseball_formulation_stacksize_5_overlap_6_lineups_4.csv");
+#path_output = string(folder,"proj_baseball_baseball_formulation_stacksize_5_overlap_6_lineups_4.csv");
+
+
+#P = read_player_data(path_hitters,path_pitchers);
+#L = readtable(path_lineups)
+
+#lineup_points_proj(path_lineups,path_hitters,path_pitchers,path_output)
